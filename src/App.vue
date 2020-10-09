8c5318b950bb88969890136d019cdcb9691ffd85
Nathan Friend <nathan@gitlab.com>
2020-10-09 19:02:07 -0400

    Rescaffold with Vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script>
 import HelloWorld from "./components/HelloWorld.vue";
 
-export default defineComponent({
+export default {
   name: "App",
   components: {
     HelloWorld
   }
-});
+};
 </script>
 
 <style lang="scss">
