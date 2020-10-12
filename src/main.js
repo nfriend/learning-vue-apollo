194f6bd1a147fb31c7d5f086de814097721e5153
Nathan Friend <nathan@gitlab.com>
2020-10-12 19:46:26 -0400

    Run tailwind Vue CLI generator

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import { apolloProvider } from './graphql/getProvider';
+import './assets/tailwind.css';
 
 Vue.config.productionTip = false;
 
